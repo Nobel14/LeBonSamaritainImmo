d15fa8fa64d8ca4257bde83b615e7b60074d568b
Nobel14 <nobel.kemmegne@gmail.com>
2023-05-26 22:34:25 +0100

    Ajout des sections 'Ville', 'Superficie' et du checkbox-filter(Tout, Titré, en vente, en location) dans la page terrain | Ajout des boutons de la pagination (visuels uniquement) |  Webresponsive de la page terrain

--- animation.js
+++ animation.js
@@ -48,4 +48,59 @@ overlay.onclick = function() {
 
     // document.querySelector(".humberger__menu__overlay").classList.add("active");
     // document.querySelector("body").classList.add("over_hid");
-  
+ 
+    // Voici le script du range slider qui va jouer le role de filtre pour la superficie
+				var rangeOne = document.querySelector('input[name="rangeOne"]'),
+						rangeTwo = document.querySelector('input[name="rangeTwo"]'),
+						outputOne = document.querySelector('.outputOne'),
+						outputTwo = document.querySelector('.outputTwo'),
+						inclRange = document.querySelector('.incl-range'),
+						updateView = function () {
+							if (this.getAttribute('name') === 'rangeOne') {
+								outputOne.innerHTML = this.value;
+								outputOne.style.left = this.value / this.getAttribute('max') * 100 + '%';
+							} if (this.getAttribute('name') === 'rangeTwo') {
+								outputTwo.style.left = this.value / this.getAttribute('max') * 100 + '%';
+								outputTwo.innerHTML = this.value
+							}
+							if (parseInt(rangeOne.value) > parseInt(rangeTwo.value)) {
+								inclRange.style.width = (rangeOne.value - rangeTwo.value) / this.getAttribute('max') * 100 + '%';
+								inclRange.style.left = rangeTwo.value / this.getAttribute('max') * 100 + '%';
+							} else {
+								inclRange.style.width = (rangeTwo.value - rangeOne.value) / this.getAttribute('max') * 100 + '%';
+								inclRange.style.left = rangeOne.value / this.getAttribute('max') * 100 + '%';
+							}
+						};
+				var rangeOne_accordion = document.querySelector('input[name="rangeOne-accordion"]'),
+						rangeTwo_accordion = document.querySelector('input[name="rangeTwo-accordion"]'),
+						outputOne_accordion = document.querySelector('.outputOne-accordion'),
+						outputTwo_accordion = document.querySelector('.outputTwo-accordion'),
+						inclRange_accordion = document.querySelector('.incl-range-accordion'),
+						updateView_accordion = function () {
+							if (this.getAttribute('name') === 'rangeOne-accordion') {
+								outputOne_accordion.innerHTML = this.value;
+								outputOne_accordion.style.left = this.value / this.getAttribute('max') * 100 + '%';
+							} if (this.getAttribute('name') === 'rangeTwo-accordion') {
+								outputTwo_accordion.style.left = this.value / this.getAttribute('max') * 100 + '%';
+								outputTwo_accordion.innerHTML = this.value
+							}
+							if (parseInt(rangeOne_accordion.value) > parseInt(rangeTwo_accordion.value)) {
+								inclRange_accordion.style.width = (rangeOne_accordion.value - rangeTwo_accordion.value) / this.getAttribute('max') * 100 + '%';
+								inclRange_accordion.style.left = rangeTwo_accordion.value / this.getAttribute('max') * 100 + '%';
+							} else {
+								inclRange_accordion.style.width = (rangeTwo_accordion.value - rangeOne_accordion.value) / this.getAttribute('max') * 100 + '%';
+								inclRange_accordion.style.left = rangeOne_accordion.value / this.getAttribute('max') * 100 + '%';
+							}
+						};
+
+					document.addEventListener('DOMContentLoaded', function () {
+						updateView.call(rangeOne);
+						updateView.call(rangeTwo);
+						updateView_accordion.call(rangeOne_accordion);
+						updateView_accordion.call(rangeTwo_accordion);
+						$('input[type="range"]').on('mouseup', function() {
+							this.blur();
+						}).on('mousedown input', function () {
+							updateView.call(this);
+						});
+					});
